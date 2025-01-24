4b439bb509578979175937ee102e70990997cf2d
Jason Ragsdale <jrags@jasrags.net>
2025-01-24 16:48:14 -0600

    progress on the table

--- cmd/iotest/main.go
+++ cmd/iotest/main.go
@@ -2,34 +2,36 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 
+	"github.com/Jasrags/NewMUD/internal/game"
 	"github.com/charmbracelet/lipgloss"
-	"github.com/charmbracelet/lipgloss/table"
-	"github.com/i582/cfmt/cmd/cfmt"
 )
 
-const (
-	ColorBlack = iota
-	ColorRed
-	ColorGreen
-	ColorYellow
-	ColorBlue
-	ColorMagenta
-	ColorCyan
-	ColorWhite
-	ColorBrightBlack
-	ColorBrightRed
-	ColorBrightGreen
-	ColorBrightYellow
-	ColorBrightBlue
-	ColorBrightMagenta
-	ColorBrightCyan
-	ColorBrightWhite
-)
+// const (
+// 	ColorBlack = iota
+// 	ColorRed
+// 	ColorGreen
+// 	ColorYellow
+// 	ColorBlue
+// 	ColorMagenta
+// 	ColorCyan
+// 	ColorWhite
+// 	ColorBrightBlack
+// 	ColorBrightRed
+// 	ColorBrightGreen
+// 	ColorBrightYellow
+// 	ColorBrightBlue
+// 	ColorBrightMagenta
+// 	ColorBrightCyan
+// 	ColorBrightWhite
+// )
 
 func main() {
-	println("Hello, World!")
+	var output strings.Builder
+	output.WriteString(RenderCharacterTable())
+	// println("Hello, World!")
 
 	// char := &game.Character{
 	// 	UserID: "test_user",
@@ -81,49 +83,49 @@ func main() {
 	// 	},
 	// }
 
-	var output strings.Builder
-	// Personal data                Core combat info
-	// 77 38| 25/25/25
-	output.WriteString(cfmt.Sprintf("╭─────────────────────────────────────────────────────────────────────────────╮\n"))
-	output.WriteString(cfmt.Sprintf("│ Name:                                                                       │\n"))
-	output.WriteString(cfmt.Sprintf("│                                                                             │\n"))
-	output.WriteString(cfmt.Sprintf("├--------------------------------------┬--------------------------------------┤\n"))
-	// output.WriteString(cfmt.Sprintf("│ %-36s │ %-36s │\n",
-	// "{{Body}}::white|bold {{3}}::cyan (5)", "{{Essence}}::white|bold 6.0"))
-	output.WriteString(cfmt.Sprintf("│ {{Body}}::white|bold                  {{4}}::cyan ({{6}}::cyan)          │ {{Essence}}::white|bold                  {{4}}::cyan ({{6}}::cyan)       │\n"))
-	output.WriteString(cfmt.Sprintf("│ {{Agility}}::white|bold               {{4}}::cyan ({{6}}::cyan)          │                                      │\n"))
-	output.WriteString(cfmt.Sprintf("│ {{Reaction}}::white|bold              {{4}}::cyan ({{6}}::cyan)          │ {{Initiative}}::white|bold               {{4}}::cyan ({{6}}::cyan) + {{1d6}}::cyan ({{3d6}}::cyan)       │\n"))
-	output.WriteString(cfmt.Sprintf("│ {{Strength}}::white|bold              {{4}}::cyan ({{6}}::cyan)          │ {{Matrix Initiative}}::white|bold        {{4}}::cyan ({{6}}::cyan) + {{1d6}}::cyan ({{3d6}}::cyan)       │\n"))
-	output.WriteString(cfmt.Sprintf("│ {{Willpower}}::white|bold             {{4}}::cyan ({{6}}::cyan)          │ {{Astral Initiative}}::white|bold        {{4}}::cyan ({{6}}::cyan) + {{1d6}}::cyan ({{3d6}}::cyan)       │\n"))
-	output.WriteString(cfmt.Sprintf("│ {{Logic}}::white|bold                 {{4}}::cyan ({{6}}::cyan)          │ {{Composure}}::white|bold                            │\n"))
-	output.WriteString(cfmt.Sprintf("│ {{Intuition}}::white|bold             {{4}}::cyan ({{6}}::cyan)          │ {{Judge Intentions}}::white|bold                     │\n"))
-	output.WriteString(cfmt.Sprintf("│ {{Charisma}}::white|bold              {{4}}::cyan ({{6}}::cyan)          │ {{Memory}}::white|bold                               │\n"))
-	output.WriteString(cfmt.Sprintf("│ {{Magic/Resonance}}::white|bold       {{4}}::cyan ({{6}}::cyan)          │ {{Lift/Carry}}::white|bold                           │\n"))
-	output.WriteString(cfmt.Sprintf("│                                      │ {{Movement}}::white|bold                             │\n"))
-	output.WriteString(cfmt.Sprintf("│                                      │                                      │\n"))
-	output.WriteString(cfmt.Sprintf("│ {{Edge Aval/Max}}::white|bold         {{4}}::cyan/{{5}}::cyan            │                                      │\n"))
-	output.WriteString(cfmt.Sprintf("├-------------------------┬------------┴------------┬-------------------------┤\n"))
-	output.WriteString(cfmt.Sprintf("│ {{Physcial Limit}}::white|bold {{6}}::cyan ({{3}}::red)    │ {{Mental Limit}}::white|bold {{6}}::cyan ({{3}}::red)      │ {{Social Limit}}::white|bold {{6}}::cyan ({{3}}::red)      │\n"))
-	output.WriteString(cfmt.Sprintf("│ {{Mental Limit}}::white|bold   {{6}}::cyan ({{3}}::red)    │ {{Mental Limit}}::white|bold {{6}}::cyan ({{3}}::red)      │ {{Social Limit}}::white|bold {{6}}::cyan ({{3}}::red)      │\n"))
-	output.WriteString(cfmt.Sprintf("│ {{Social Limit}}::white|bold   {{6}}::cyan ({{3}}::red)    │ {{Mental Limit}}::white|bold {{6}}::cyan ({{3}}::red)      │ {{Social Limit}}::white|bold {{6}}::cyan ({{3}}::red)      │\n"))
-	output.WriteString(cfmt.Sprintf("│                         │                         │                         │\n"))
-	output.WriteString(cfmt.Sprintf("│                         │                         │                         │\n"))
-	output.WriteString(cfmt.Sprintf("╰─────────────────────────┴─────────────────────────┴─────────────────────────╯\n"))
-	output.WriteString("\n")
-	n := lipgloss.NewStyle().Foreground(lipgloss.Color("240")).Padding(0, 1, 0, 1).Render
+	// // Personal data                Core combat info
+	// // 77 38| 25/25/25
+	// output.WriteString(cfmt.Sprintf("╭─────────────────────────────────────────────────────────────────────────────╮\n"))
+	// output.WriteString(cfmt.Sprintf("│ Name:                                                                       │\n"))
+	// output.WriteString(cfmt.Sprintf("│                                                                             │\n"))
+	// output.WriteString(cfmt.Sprintf("├--------------------------------------┬--------------------------------------┤\n"))
+	// // output.WriteString(cfmt.Sprintf("│ %-36s │ %-36s │\n",
+	// // "{{Body}}::white|bold {{3}}::cyan (5)", "{{Essence}}::white|bold 6.0"))
+	// output.WriteString(cfmt.Sprintf("│ {{Body}}::white|bold                  {{4}}::cyan ({{6}}::cyan)          │ {{Essence}}::white|bold                  {{4}}::cyan ({{6}}::cyan)       │\n"))
+	// output.WriteString(cfmt.Sprintf("│ {{Agility}}::white|bold               {{4}}::cyan ({{6}}::cyan)          │                                      │\n"))
+	// output.WriteString(cfmt.Sprintf("│ {{Reaction}}::white|bold              {{4}}::cyan ({{6}}::cyan)          │ {{Initiative}}::white|bold               {{4}}::cyan ({{6}}::cyan) + {{1d6}}::cyan ({{3d6}}::cyan)       │\n"))
+	// output.WriteString(cfmt.Sprintf("│ {{Strength}}::white|bold              {{4}}::cyan ({{6}}::cyan)          │ {{Matrix Initiative}}::white|bold        {{4}}::cyan ({{6}}::cyan) + {{1d6}}::cyan ({{3d6}}::cyan)       │\n"))
+	// output.WriteString(cfmt.Sprintf("│ {{Willpower}}::white|bold             {{4}}::cyan ({{6}}::cyan)          │ {{Astral Initiative}}::white|bold        {{4}}::cyan ({{6}}::cyan) + {{1d6}}::cyan ({{3d6}}::cyan)       │\n"))
+	// output.WriteString(cfmt.Sprintf("│ {{Logic}}::white|bold                 {{4}}::cyan ({{6}}::cyan)          │ {{Composure}}::white|bold                            │\n"))
+	// output.WriteString(cfmt.Sprintf("│ {{Intuition}}::white|bold             {{4}}::cyan ({{6}}::cyan)          │ {{Judge Intentions}}::white|bold                     │\n"))
+	// output.WriteString(cfmt.Sprintf("│ {{Charisma}}::white|bold              {{4}}::cyan ({{6}}::cyan)          │ {{Memory}}::white|bold                               │\n"))
+	// output.WriteString(cfmt.Sprintf("│ {{Magic/Resonance}}::white|bold       {{4}}::cyan ({{6}}::cyan)          │ {{Lift/Carry}}::white|bold                           │\n"))
+	// output.WriteString(cfmt.Sprintf("│                                      │ {{Movement}}::white|bold                             │\n"))
+	// output.WriteString(cfmt.Sprintf("│                                      │                                      │\n"))
+	// output.WriteString(cfmt.Sprintf("│ {{Edge Aval/Max}}::white|bold         {{4}}::cyan/{{5}}::cyan            │                                      │\n"))
+	// output.WriteString(cfmt.Sprintf("├-------------------------┬------------┴------------┬-------------------------┤\n"))
+	// output.WriteString(cfmt.Sprintf("│ {{Physcial Limit}}::white|bold {{6}}::cyan ({{3}}::red)    │ {{Mental Limit}}::white|bold {{6}}::cyan ({{3}}::red)      │ {{Social Limit}}::white|bold {{6}}::cyan ({{3}}::red)      │\n"))
+	// output.WriteString(cfmt.Sprintf("│ {{Mental Limit}}::white|bold   {{6}}::cyan ({{3}}::red)    │ {{Mental Limit}}::white|bold {{6}}::cyan ({{3}}::red)      │ {{Social Limit}}::white|bold {{6}}::cyan ({{3}}::red)      │\n"))
+	// output.WriteString(cfmt.Sprintf("│ {{Social Limit}}::white|bold   {{6}}::cyan ({{3}}::red)    │ {{Mental Limit}}::white|bold {{6}}::cyan ({{3}}::red)      │ {{Social Limit}}::white|bold {{6}}::cyan ({{3}}::red)      │\n"))
+	// output.WriteString(cfmt.Sprintf("│                         │                         │                         │\n"))
+	// output.WriteString(cfmt.Sprintf("│                         │                         │                         │\n"))
+	// output.WriteString(cfmt.Sprintf("╰─────────────────────────┴─────────────────────────┴─────────────────────────╯\n"))
+	// output.WriteString("\n")
+	// n := lipgloss.NewStyle().Foreground(lipgloss.Color("240")).Padding(0, 1, 0, 1).Render
 	// v := lipgloss.NewStyle().Foreground(lipgloss.Color("240")).Padding(0, 1, 0, 1).Render
 	// cellStyle := lipgloss.NewStyle().Padding(0, 1, 0, 1).Render
 
-	t := table.New().Width(80)
-	t.Row(n("Name"))
-	t.Row(n("Body"), n("Essence"))
-	// // t.Row("Bubble Tea", s("Milky"))
-	// // t.Row("Milk Tea", s("Also milky"))
-	// // t.Row("Actual milk", s("Milky as well"))
-	output.WriteString(t.Render())
+	// t := table.New().Width(80)
+	// t.Row(n("Name"))
+	// t.Row(n("Body"), n("Essence"))
+	// // // t.Row("Bubble Tea", s("Milky"))
+	// // // t.Row("Milk Tea", s("Also milky"))
+	// // // t.Row("Actual milk", s("Milky as well"))
+	// output.WriteString(t.Render())
 
-	output.WriteString("\n\n")
-	output.WriteString(RenderANSI16Colors())
+	// output.WriteString("\n\n")
+	// output.WriteString(RenderANSI16Colors())
+	// output.WriteString(RenderTable())
 
 	// fmt.Println(t.Render())
 	// Attributes                   Conditon monitor
@@ -318,4 +320,216 @@ func RenderANSI16Colors() string {
 	}
 
 	return output
+}
+
+// {0, "Black"}, {1, "Red"}, {2, "Green"}, {3, "Yellow"},
+//
+//	{4, "Blue"}, {5, "Magenta"}, {6, "Cyan"}, {7, "White"},
+//
+// │ Name: Shadow Walker                                                            │
+var (
+	borderStyle = lipgloss.NewStyle().
+			BorderStyle(lipgloss.NormalBorder()).
+			BorderForeground(lipgloss.Color("63")).
+			Padding(0, 1, 0, 1)
+
+		// Table formatting
+	singleColumnStyle = borderStyle.Width(80)
+	dualColumnStyle   = borderStyle.Width(39)
+
+	// Text styles
+	attrNameStyle   = lipgloss.NewStyle().Bold(true).Foreground(white)
+	attrValueStyle  = lipgloss.NewStyle().Bold(false).Foreground(cyan)
+	attrPosModStyle = lipgloss.NewStyle().Bold(true).Foreground(green)
+	attrNegModStyle = lipgloss.NewStyle().Bold(true).Foreground(red)
+
+	// Colors
+	black   = lipgloss.Color("0")
+	red     = lipgloss.Color("1")
+	green   = lipgloss.Color("2")
+	yellow  = lipgloss.Color("3")
+	blue    = lipgloss.Color("4")
+	magenta = lipgloss.Color("5")
+	cyan    = lipgloss.Color("6")
+	white   = lipgloss.Color("7")
+)
+
+var attrs = game.NewAttributes()
+
+// RenderAttributes renders the attributes of a character.
+func RenderAttributes(attrs game.Attributes) []string {
+	var strs []string
+
+	strs = append(strs, RenderAttribute(attrs.Body))
+	strs = append(strs, RenderAttribute(attrs.Agility))
+	strs = append(strs, RenderAttribute(attrs.Reaction))
+	strs = append(strs, RenderAttribute(attrs.Strength))
+	strs = append(strs, RenderAttribute(attrs.Willpower))
+	strs = append(strs, RenderAttribute(attrs.Logic))
+	strs = append(strs, RenderAttribute(attrs.Intuition))
+	strs = append(strs, RenderAttribute(attrs.Charisma))
+	strs = append(strs, RenderAttribute(attrs.Essence))
+	strs = append(strs, RenderAttribute(attrs.Magic))
+	strs = append(strs, RenderAttribute(attrs.Resonance))
+
+	// Now we want to remove all the empty strings from the slice
+	for i, s := range strs {
+		if s == "" {
+			strs = append(strs[:i], strs[i+1:]...)
+		}
+
+	}
+
+	return strs
+}
+
+// RenderAttribute renders a single attribute for display.
+func RenderAttribute[T int | float64](attr game.Attribute[T]) string {
+	var output strings.Builder
+
+	if attr.Base == 0 {
+		return ""
+	}
+
+	output.WriteString(attrNameStyle.Render(fmt.Sprintf("%-10s", attr.Name)))
+	output.WriteString(attrValueStyle.Render(fmt.Sprintf(" %-2v", renderValue(attr.Base))))
+	if attr.TotalValue != attr.Base {
+		style := attrPosModStyle
+		if attr.TotalValue < attr.Base {
+			style = attrNegModStyle
+		}
+		output.WriteString(style.Render(fmt.Sprintf(" (%v)", renderValue(attr.TotalValue))))
+	}
+
+	return output.String()
+}
+
+// renderValue formats the value of an attribute for display.
+func renderValue[T int | float64](value T) string {
+	switch v := any(value).(type) {
+	case int:
+		return strconv.Itoa(v)
+	case float64:
+		return fmt.Sprintf("%.2f", v)
+	default:
+		return fmt.Sprintf("%v", v)
+	}
+}
+
+// RenderCharacterTable renders the entire character table.
+func RenderCharacterTable() string {
+	attrs.Body.SetBase(10)
+	attrs.Body.AddDelta(2)
+	attrs.Agility.SetBase(4)
+	attrs.Reaction.SetBase(4)
+	attrs.Strength.SetBase(4)
+	attrs.Strength.SubDelta(2)
+	attrs.Willpower.SetBase(4)
+	attrs.Logic.SetBase(4)
+	attrs.Intuition.SetBase(4)
+	attrs.Charisma.SetBase(4)
+	attrs.Essence.SetBase(6.0)
+	attrs.Essence.SubDelta(0.5)
+	// attrs.Magic.SetBase(4)
+	attrs.Resonance.SetBase(4)
+	attrs.Recalculate()
+
+	strs := RenderAttributes(attrs)
+	table := lipgloss.JoinVertical(lipgloss.Left,
+		// Personal Data
+		singleColumnStyle.Render("Name: Shadow Walker"),
+		// Metatype, Ethnicity, Age, Sex, Height, Weight, Street Cred, Notoriety, Public Awareness, Karma, Total Karma
+		lipgloss.JoinHorizontal(lipgloss.Top,
+			lipgloss.JoinVertical(lipgloss.Left,
+				// Attributes left
+				// All true attributes
+				dualColumnStyle.Render(
+					lipgloss.JoinVertical(lipgloss.Left,
+						strs...,
+					),
+				),
+			),
+			// Attributes right
+			// Derivied attributes - Iniitiative, Matrix Initiative, Astral Initiative, Composure, Judge Intentions, Memory, Lift/Carry, Movement
+			lipgloss.JoinVertical(lipgloss.Left,
+				dualColumnStyle.Render(
+					lipgloss.JoinVertical(lipgloss.Left,
+						attrNameStyle.Render("Initiative"),
+						attrNameStyle.Render("Matrix Initiative"),
+						attrNameStyle.Render("Astral Initiative"),
+						attrNameStyle.Render("Composure"),
+						attrNameStyle.Render("Judge Intentions"),
+						attrNameStyle.Render("Memory"),
+						attrNameStyle.Render("Lift/Carry"),
+						attrNameStyle.Render("Movement"),
+					),
+				),
+			),
+		),
+	)
+
+	return table
+
+	// table := lipgloss.JoinVertical(lipgloss.Left,
+	// 	singleColumnStyle.Render("Name: Shadow Walker"),
+
+	// 	lipgloss.JoinHorizontal(lipgloss.Top,
+	// 		dualColumnStyle.Render(
+	// 			// dualColumnStyle.Render(RenderAttributes(attrs)),
+	// 			// output.WriteString(RenderAttribute(attrs.Body))
+	// 			RenderAttribute(attrs.Agility),
+	// 			RenderAttribute(attrs.Reaction),
+	// 			RenderAttribute(attrs.Strength),
+	// 			RenderAttribute(attrs.Willpower),
+	// 			RenderAttribute(attrs.Logic),
+	// 			RenderAttribute(attrs.Intuition),
+	// 			RenderAttribute(attrs.Charisma),
+	// 			RenderAttribute(attrs.Essence),
+	// 			RenderAttribute(attrs.Magic),
+	// 			RenderAttribute(attrs.Resonance),
+	// 		),
+	// 		dualColumnStyle.Render("empty"),
+	// 	),
+	// 	// dualColumnStyle.Render("empty"),
+	// )
+
+	// // Styles for table components
+	// headerStyle := lipgloss.NewStyle().
+	// 	BorderStyle(lipgloss.NormalBorder()).
+	// 	BorderBottom(true).
+	// 	Width(60).
+	// 	Padding(0, 1).
+	// 	Bold(true)
+
+	// dualColumnStyle := lipgloss.NewStyle().
+	// 	Width(30).
+	// 	Padding(0, 1)
+
+	// divider := lipgloss.NewStyle().
+	// 	BorderStyle(lipgloss.NormalBorder()).
+	// 	BorderBottom(true).
+	// 	Render("────────────────────────────────────────────")
+
+	// // Header row
+	// header := headerStyle.Render("Name: Shadow Walker")
+
+	// // Dual-column rows
+	// bodyColumn := dualColumnStyle.Render("Body: 4 (6)")
+	// essenceColumn := dualColumnStyle.Render("Essence: 4 (6)")
+
+	// agilityColumn := dualColumnStyle.Render("Agility: 4 (6)")
+	// emptyColumn := dualColumnStyle.Render("") // Blank column
+
+	// dualRow1 := lipgloss.JoinHorizontal(lipgloss.Top, bodyColumn, essenceColumn)
+	// dualRow2 := lipgloss.JoinHorizontal(lipgloss.Top, agilityColumn, emptyColumn)
+
+	// // Combine everything into a single table
+	// table := lipgloss.JoinVertical(lipgloss.Left,
+	// 	header,
+	// 	divider,
+	// 	dualRow1,
+	// 	dualRow2,
+	// )
+
+	return table
 }
